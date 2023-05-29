fce4e789f81a3dc6c3110ef5960d3701f19db6d6
MouraDWG <guilherme.moura@sou.inteli.edu.br>
2023-05-28 21:38:10 -0300

    Commit Página cadastrar e dashboard feitas

--- src/pages/cadastro_equipamentos.tsx
+++ src/pages/cadastro_equipamentos.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Navbar } from '../components/Navbar'
+import { CadastrarEquipamentos } from '../components/CadastrarDevice'
 
 const CadastroEquipamentos= () => {
     return(
         <div>
             <Navbar />
+            <div>
+                <CadastrarEquipamentos />
+            </div>
         </div>
     )
 }
